37c1a01b6f0ad58f988be2319d715c4154be0c52
Makeda P Mckune <makedamckune@gmail.com>
2019-11-06 23:18:08 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -1,5 +1,9 @@
 #values
+<<<<<<< HEAD
 
+=======
+require_relative "../lib/values.rb"
+>>>>>>> b5e1ab4df083ea9052247b3199a8eea2fe1cd344
 def get_the_values
   groceries = {
     { :dairy=> ["milk", "yogurt", "cheese"],
